40e3c3fa5e29229c6dc1ebc0fded08da025f7946
mkidson <miles.kidson@gmail.com>
2023-10-31 10:18:30 +0200

    moved module to src

---
+++ src/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pulse_analysis',
+      version='1.0',
+      description='pulse_analysis',
+      author='Miles Kidson',
+      author_email='kdsmil001@myuct.ac.za',
+      py_modules=['pulse_analysis']
+     )
